463a8c1a6f51df0685ce5e0b73092558bf6278c6
gaeunnlee <gaeunnnleee@gmail.com>
2024-10-13 23:49:50 +0900

    solve: 24.10.13

---
+++ 프로그래머스/[042478] K번째 수.js
@@ -0,0 +1,7 @@
+function solution(array, commands) {
+  const answer = [];
+  for (const [i, j, k] of commands) {
+    answer.push(array.slice(i - 1, j).sort((a, b) => a - b)[k-1]);
+  }
+  return answer
+}
